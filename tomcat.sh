8c2c5d481ef212ee921c637fe525394e8f5d5b77
rangurahul <120587828+RanguRahul@users.noreply.github.com>
2023-04-08 13:22:25 +0530

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,13 @@
+yum install -y tomcat7
+
+which tomcat7
+
+service tomcat7 start
+
+cd /usr/share/tomcat7/
+
+cd webapps/
+
+netstat -tulnp ( To check ports is listning ports )
+
+Tomcat8 - sudo yum install tomcat8 tomcat8-webapps chkconfig tomcat8 on
